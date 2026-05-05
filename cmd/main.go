967b0d138f8fbe697838cba14e22d604375a6ac5
Hugo Marx <marx.hugo@gmail.com>
2026-05-05 22:42:04 +0200

    base md display - WIP

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 
 	"symdoc/internal/config"
 	"symdoc/internal/repo"
+
+	"github.com/charmbracelet/glamour"
 )
 
 func main() {
 	fmt.Println("Hello Symdoc")
+
+	fileName := os.Args[1:][0] // Starts at index 1 since 0 is the program path.
 	config := config.Load()
 
 	if !config.MdGenerated {
@@ -19,4 +25,16 @@ func main() {
 	} else {
 		log.Print("Already converted !")
 	}
+
+	fileContent, err := os.ReadFile(filepath.Join(os.ExpandEnv(config.AssetsDir), fileName))
+	if err != nil {
+		log.Fatal("File not found : ", err.Error())
+	}
+
+	output, err := glamour.Render(string(fileContent), "dark")
+	if err != nil {
+		log.Fatal("Unable to render file content : ", err.Error())
+	}
+
+	fmt.Print(output)
 }
