b9b08ca631ea5b17586377303c94fe62e584e62a
Hugo Marx <marx.hugo@gmail.com>
2026-05-10 22:16:33 +0200

    add pager + style test

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,73 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"symdoc/internal/config"
 	"symdoc/internal/repo"
 
+	"charm.land/bubbles/v2/help"
+	"charm.land/bubbles/v2/viewport"
+	tea "charm.land/bubbletea/v2"
+	"charm.land/lipgloss/v2"
+
 	"github.com/charmbracelet/glamour"
 )
 
+type model struct {
+	width    int
+	height   int
+	filePath string
+	content  string
+	viewport viewport.Model
+	help     help.Model
+}
+
+var (
+	titleStyle = func() lipgloss.Style {
+		b := lipgloss.BlockBorder()
+		return lipgloss.NewStyle().BorderStyle(b).Padding(1, 5).Margin(1, 0)
+	}()
+
+	infoStyle = func() lipgloss.Style {
+		b := lipgloss.RoundedBorder()
+		b.Left = "┤"
+		return lipgloss.NewStyle().BorderStyle(b).Padding(0, 1).BorderStyle(b)
+	}()
+)
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c", "esc":
+			return m, tea.Quit
+		default:
+			var cmd tea.Cmd
+			m.viewport, cmd = m.viewport.Update(msg)
+			return m, cmd
+		}
+	default:
+		return m, nil
+	}
+}
+
+func (m model) View() tea.View {
+	view := fmt.Sprintf("%s\n%s\n%s", m.headerView(), m.viewport.View(), m.footerView())
+	return tea.NewView(view)
+}
+
 func main() {
-	fmt.Println("Hello Symdoc")
-
-	fileName := os.Args[1:][0] // Starts at index 1 since 0 is the program path.
 	config := config.Load()
-
 	if !config.MdGenerated {
 		if err := repo.BuildMarkdown(config.AssetsDir); err != nil {
 			log.Fatal(err)
@@ -26,7 +76,10 @@ func main() {
 		log.Print("Already converted !")
 	}
 
-	fileContent, err := os.ReadFile(filepath.Join(os.ExpandEnv(config.AssetsDir), fileName))
+	fileName := os.Args[1:][0] // Starts at index 1 since 0 is the program path.
+
+	filePath := filepath.Join(os.ExpandEnv(config.AssetsDir), fileName)
+	fileContent, err := os.ReadFile(filePath)
 	if err != nil {
 		log.Fatal("File not found : ", err.Error())
 	}
@@ -36,5 +89,51 @@ func main() {
 		log.Fatal("Unable to render file content : ", err.Error())
 	}
 
-	fmt.Print(output)
+	vp := viewport.New(viewport.WithWidth(90), viewport.WithHeight(30)) // largeur, hauteur
+	vp.SetContent(output)
+
+	p := tea.NewProgram(model{
+		filePath: filePath,
+		content:  output,
+		viewport: vp,
+	})
+
+	if _, err := p.Run(); err != nil {
+		fmt.Println("could not run program:", err)
+		os.Exit(1)
+	}
+}
+
+func (m model) headerView() string {
+	mainTitle := getMainTitle(m.filePath)
+
+	title := titleStyle.Render(mainTitle)
+	line := strings.Repeat(" ", max(0, (m.viewport.Width()/2)-lipgloss.Width(title)))
+	return lipgloss.JoinHorizontal(lipgloss.Center, line, title)
+}
+
+func (m model) footerView() string {
+	info := infoStyle.Render(fmt.Sprintf("%.f%%", m.viewport.ScrollPercent()*100))
+	line := strings.Repeat("-", max(0, m.viewport.Width()-lipgloss.Width(info)))
+	return lipgloss.JoinHorizontal(lipgloss.Center, line, info)
+}
+
+func getMainTitle(filePath string) string {
+	reader, err := os.Open(filePath)
+	if err != nil {
+		return "undefined"
+	}
+
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), "title") {
+			return strings.TrimSpace(strings.Replace(scanner.Text(), "title: ", "", 1))
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return ""
+	}
+
+	return ""
 }
--- internal/repo/repo.go
+++ internal/repo/repo.go
@@ -33,7 +33,7 @@ func BuildMarkdown(root string) error {
 func rstToMd(path string, d fs.DirEntry, err error) error {
 	if filepath.Ext(path) == ".rst" {
 		markdownFile := strings.Replace(d.Name(), ".rst", ".md", 1)
-		if err := exec.Command("pandoc", path, "-f", "rst", "-t", "markdown", "-s", "-o", fmt.Sprint(filepath.Join(filepath.Dir(path), markdownFile))).Run(); err != nil {
+		if err := exec.Command("pandoc", path, "-f", "rst", "-t", "markdown", "-o", fmt.Sprint(filepath.Join(filepath.Dir(path), markdownFile))).Run(); err != nil {
 			return err
 		}
 		fmt.Println(fmt.Sprint(filepath.Dir(path), "/", markdownFile, " created."))
